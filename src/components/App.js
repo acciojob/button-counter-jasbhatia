ecf58622703d85f0a25550752c71794012cb2f11
jasbhatia <94101161+jasbhatia@users.noreply.github.com>
2023-06-29 19:15:42 +0530

    submitting code-Thu Jun 29 2023 19:15:31 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,18 @@
-
 import React from "react";
-import './../styles/App.css';
+import "./../styles/App.css";
 
 const App = () => {
+  const [count, setCount] = useState(0);
+
+  function inc() {
+    setCount(count + 1);
+  }
   return (
     <div>
-        {/* Do not remove the main div */}
+      <p>Button clicked {count} times.</p>
+      <button onclick={inc}>Click me</button>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
